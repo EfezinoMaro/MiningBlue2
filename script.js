e8825e26ab7081c4086cd812ce5b205b3567bcc0
Efezino Elvis Maro <efezinomaro228@gmail.com>
2022-09-21 04:48:46 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+
+  document.getElementById("text11").style.display="block"
+  document.getElementById("text22").style.display="none"
+  document.getElementById("text33").style.display="none"
+  function History(){
+      document.getElementById("active11").style.backgroundColor="rgb(0, 73, 133)"
+      document.getElementById("active22").style.backgroundColor="grey"
+      document.getElementById("active33").style.backgroundColor="grey"
+    document.getElementById("text11").style.display="block"
+    document.getElementById("text22").style.display="none"
+    document.getElementById("text33").style.display="none" 
+  }
+  function Mission(){
+    document.getElementById("active22").style.backgroundColor="rgb(0, 73, 133)"
+    document.getElementById("active11").style.backgroundColor="grey"
+    document.getElementById("active33").style.backgroundColor="grey"
+    document.getElementById("text11").style.display="none"
+    document.getElementById("text22").style.display="block"
+    document.getElementById("text33").style.display="none"
+     
+  }
+  function Vision(){
+    document.getElementById("active33").style.backgroundColor="rgb(0, 73, 133)"
+    document.getElementById("active22").style.backgroundColor="grey"
+    document.getElementById("active11").style.backgroundColor="grey"
+    document.getElementById("text11").style.display="none"
+    document.getElementById("text22").style.display="none"
+    document.getElementById("text33").style.display="block"
+     
+  }
+  // =======================NavBar =====================================================//====================//
